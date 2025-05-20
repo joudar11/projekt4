177bc0fae8fdd4ddb597630793d05b4aac6ce2fd
joudar11 <krystofklika@pm.me>
2025-05-20 14:31:09 +0200

    define functions

---
+++ main.py
@@ -0,0 +1,22 @@
+def hlavni_menu():
+    pass
+
+
+def pridat_ukol():
+    pass
+
+
+def zobrazit_ukoly():
+    pass
+
+
+def odstranit_ukol():
+    pass
+
+
+def run():
+    pass
+
+
+if __name__ == "__main__":
+    run()
